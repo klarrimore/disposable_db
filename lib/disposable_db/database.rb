7ee3351b7ba014466bcb4f8614bc4f7e2af6f82b
Keith Larrimore <keithlarrimore@gmail.com>
2012-03-27 13:49:23 -0400

    added primary_key

--- lib/disposable_db/database.rb
+++ lib/disposable_db/database.rb
@@ -8,16 +8,24 @@ module DisposableDB
 
     def create_table!(table_template)
       @connection.create_table! table_template.name.to_sym do
+        primary_key table_template.primary_key if table_template.primary_key 
         table_template.columns.each do |column|
           column column.name.to_sym, column.type
+        end
+        table_template.indexes.each do |idx|
+          index idx.to_sym
         end
       end
     end
 
     def create_table(table_template)
       @connection.create_table? table_template.name.to_sym do
+        primary_key table_template.primary_key if table_template.primary_key 
         table_template.columns.each do |column|
           column column.name.to_sym, column.type
+        end
+        table_template.indexes.each do |idx|
+          index idx.to_sym
         end
       end
     end
