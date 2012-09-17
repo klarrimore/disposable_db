a40ad2cca71be85be748eb960739683cd4fbc8bb
klarrimore <klarrimore@icehook.com>
2012-09-17 04:01:02 -0400

    minor else catch

--- lib/disposable_db/databases/sqlite.rb
+++ lib/disposable_db/databases/sqlite.rb
@@ -7,6 +7,8 @@ module DisposableDB
           connection = args[:connection]
         elsif args[:db_path]
           connection = Sequel.sqlite args[:db_path]
+        else
+          connection = Sequel.sqlite
         end
 
         super(connection)
